511893c65dab485277bb6aef24ecfd626af11f0a
mathilde4-def <mathilde-def@hotmail.fr>
2026-07-09 14:20:29 +0200

    Add QR code generator for characters
    This script generates QR codes for each character in the 'personnages' array, displaying their name and ID.

---
+++ js/qr.js
@@ -0,0 +1,86 @@
+// =====================================
+// INQUISITIO
+// Générateur de QR Codes
+// Version 0.1
+// =====================================
+
+
+function generer(){
+
+
+    const zone = document.getElementById("zone");
+
+
+    zone.innerHTML = "";
+
+
+
+    for(let id in personnages){
+
+
+        let bloc = document.createElement("div");
+
+        bloc.className = "qr";
+
+
+
+        let titre = document.createElement("h3");
+
+        titre.innerHTML =
+        personnages[id].nom
+        +
+        "<br>"
+        +
+        id;
+
+
+
+        let qr = document.createElement("div");
+
+
+        qr.id = "qr-" + id;
+
+
+
+        bloc.appendChild(titre);
+
+        bloc.appendChild(qr);
+
+
+        zone.appendChild(bloc);
+
+
+
+        // Création du lien
+
+        let adresse =
+
+        window.location.origin
+        +
+        "/scan.html?id="
+        +
+        id;
+
+
+
+        new QRCode(
+
+            qr,
+
+            {
+
+            text:adresse,
+
+            width:150,
+
+            height:150
+
+            }
+
+        );
+
+
+    }
+
+
+}
