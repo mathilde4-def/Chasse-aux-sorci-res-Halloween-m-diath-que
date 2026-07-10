24f0b8d6884e60e681411175883865509287d2d6
mathilde4-def <mathilde-def@hotmail.fr>
2026-07-10 12:50:51 +0200

    Refactor Firebase configuration and imports
    Updated Firebase configuration with new API key and removed duplicate lines.

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,47 @@
+// ========================================
+// INQUISITIO
 // Configuration Firebase
+// Pack 1
+// ========================================
+
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+
 import {
-  getFirestore
+    getFirestore
 } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 
+
+
+// Configuration du projet Firebase
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAGkWiSUykzzGuz_PgcVTfVeFLkHB9Mj3Q",
-  authDomain: "inquisitio-halloween.firebaseapp.com",
-  projectId: "inquisitio-halloween",
-  storageBucket: "inquisitio-halloween.firebasestorage.app",
-  messagingSenderId: "191062565005",
-  appId: "1:191062565005:web:54b0eb60e2bf81d52282c4"
+
+    apiKey: "AIzaSyAGkWiSUykzzGuz_PgcVTfVeFLkHB9Mj3",
+
+    authDomain: "inquisitio-halloween.firebaseapp.com",
+
+    projectId: "inquisitio-halloween",
+
+    storageBucket: "inquisitio-halloween.firebasestorage.app",
+
+    messagingSenderId: "191062565005",
+
+    appId: "1:191062565005:web:54b0eb60e2bf81d52282c4"
+
 };
 
+
+
+// Initialisation Firebase
+
 const app = initializeApp(firebaseConfig);
+
+
+// Connexion Firestore
+
 const db = getFirestore(app);
+
+
 
 export { db };
